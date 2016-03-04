a1ab8ec3a8849cd7dc31eba027ef348104135784
coderforduty <coderforduty@github..com>
2016-03-04 21:44:37 +0800

    modified some files

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ var app = express();
 
 app.use(express.static('public'));
 
-app.get('/', function (req, res) {
+app.get('/user', function (req, res) {
+	res.sendFile('/home/ywj/express/user.html');
+});
+
+app.get('/index', function (req, res) {
 	res.sendFile('/home/ywj/express/index.html');
-})
+});
 
 app.listen(8080, function () {
 	console.log('running at 8080');
